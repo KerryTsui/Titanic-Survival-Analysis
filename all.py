6cbf7e69819059a6273c9534fd876dc43556b798
KerryTsui <32556151+KerryTsui@users.noreply.github.com>
2017-10-05 23:49:38 -0400

    lib

--- all.py
+++ all.py
@@ -72,6 +72,6 @@ logreg = LogisticRegression().fit(train_x, train_y)
 test_features = ['Sex', 'Age', 'Pclass', 'Parch', 'SibSp']
 test_x = test_data.loc[:, test_features]
 pred_y = logreg.predict(test_x)
-prediction = test_data['PassengerId']
-prediction['pred_survive'] = pred_y
+prediction = test_data
+prediction['predict_survival'] = pred_y
 prediction.to_csv('/Users/jiayicui/Desktop/Titanic Survival Analysis/output/prediction.csv')
